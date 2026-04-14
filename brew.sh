0657b8bf4146b315b690a5633a40e4df6e5b6f79
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2026-04-14 20:20:30 +0900

    brew: hammerspoon, raycast

--- brew.sh
+++ brew.sh
@@ -12,7 +12,7 @@ brew update
 brew upgrade
 
 brew install vim git gh git-delta tmux ghq ripgrep bat fd trash jq yq fzf sheldon starship mise overmind kubectx
-brew install --cask 1password-cli ghostty font-fira-code-nerd-font
+brew install --cask 1password-cli ghostty hammerspoon raycast font-fira-code-nerd-font
 brew cleanup
 
 fi
