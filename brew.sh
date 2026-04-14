7ba07ec3b87e764f496477621291cc968592fd92
Kazuyuki SUZUKI <kechol@users.noreply.github.com>
2026-04-14 20:14:40 +0900

    brew: install

--- brew.sh
+++ brew.sh
@@ -11,8 +11,8 @@ brew doctor
 brew update
 brew upgrade
 
-brew install vim git gh git-delta tmux ghq ripgrep bat fd trash jq fzf sheldon mise kubectx d-kuro/tap/gwq
-brew install --cask 1password-cli ghostty font-hackgen
+brew install vim git gh git-delta tmux ghq ripgrep bat fd trash jq yq fzf sheldon starship mise overmind kubectx
+brew install --cask 1password-cli ghostty font-fira-code-nerd-font
 brew cleanup
 
 fi
